14f7030dd587dd51562642a1b798cd5cf68c8057
Facundo Fierro <fierrof_47@hotmail.com>
2023-08-22 23:53:12 +0200

    asdasd

--- extension.js
+++ extension.js
@@ -1,24 +1,44 @@
 const vscode = require('vscode');
 
+function formatNewEntryLine(line) {
+    return line.text.trimStart();
+}
+
+function formatTypeLine(line) {
+    return '\t' + line.text.trimStart();
+}
+
+function formatDataLine(line) {
+    return '\t\t' + line.text.trimStart();
+}
+
 function activate(context) {
-    context.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider('bg3txt', {
-        provideDocumentFormattingEdits(document) {
-            const edits = [];
+    context.subscriptions.push(
+        vscode.languages.registerDocumentFormattingEditProvider('bg3txt', {
+            provideDocumentFormattingEdits(document) {
+                const edits = [];
 
-            for (let i = 0; i < document.lineCount; i++) {
-                const line = document.lineAt(i);
-                if (line.text.startsWith('new entry')) {
-                    edits.push(vscode.TextEdit.replace(line.range, line.text.trimStart()));
-                } else if (line.text.startsWith('type')) {
-                    edits.push(vscode.TextEdit.replace(line.range, '\t' + line.text.trimStart()));
-                } else if (line.text.startsWith('data')) {
-                    edits.push(vscode.TextEdit.replace(line.range, '\t\t' + line.text.trimStart()));
+                for (let i = 0; i < document.lineCount; i++) {
+                    const line = document.lineAt(i);
+                    let newText = "";
+
+                    if (line.text.startsWith('new entry')) {
+                        newText = formatNewEntryLine(line);
+                    } else if (line.text.startsWith('type')) {
+                        newText = formatTypeLine(line);
+                    } else if (line.text.startsWith('data')) {
+                        newText = formatDataLine(line);
+                    }
+
+                    if (newText) {
+                        edits.push(vscode.TextEdit.replace(line.range, newText));
+                    }
                 }
-            }
 
-            return edits;
-        }
-    }));
+                return edits;
+            }
+        })
+    );
 }
 
 exports.activate = activate;
