07465d46ee51798284e2b2bbe9539b0820ab764a
Facundo Fierro <fierrof_47@hotmail.com>
2023-08-23 13:08:16 +0200

    polished as much as i could

--- extension.js
+++ extension.js
@@ -1,43 +1,60 @@
 const vscode = require('vscode');
 
-async function activate(context) {
-    try {
-        // Register a document formatting edit provider for 'bg3txt' language
-        context.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider('bg3txt', {
-            async provideDocumentFormattingEdits(document) {
-                const edits = [];
+function activate(context) {
+    // Register the document formatting edit provider
+    context.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider('bg3txt', {
+        provideDocumentFormattingEdits(document) {
+            const edits = [];
 
-                for (let i = 0; i < document.lineCount; i++) {
-                    const line = document.lineAt(i);
-                    const lineText = line.text.trimStart();
+            for (let i = 0; i < document.lineCount; i++) {
+                const line = document.lineAt(i);
+                const lineText = line.text.trimStart();
 
-                    // Handle lines that start with 'new entry'
-                    if (lineText.startsWith('new entry')) {
-                        edits.push(vscode.TextEdit.replace(line.range, lineText));
-                    }
-                    // Handle lines that start with 'type'
-                    else if (lineText.startsWith('type')) {
-                        edits.push(vscode.TextEdit.replace(line.range, '\t' + lineText));
-                    }
-                    // Handle lines that start with 'data'
-                    else if (lineText.startsWith('data')) {
-                        edits.push(vscode.TextEdit.replace(line.range, '\t\t' + lineText));
-                    }
-
-                    // If your logic evolves to include async operations, you can await them here
-                    // await someAsyncFunction();
+                if (lineText.startsWith('new entry')) {
+                    edits.push(vscode.TextEdit.replace(line.range, lineText));
+                } else if (lineText.startsWith('type')) {
+                    edits.push(vscode.TextEdit.replace(line.range, '\t' + lineText));
+                } else if (lineText.startsWith('data')) {
+                    edits.push(vscode.TextEdit.replace(line.range, '\t\t' + lineText));
                 }
-
-                return edits;
             }
-        }));
-    } catch (error) {
-        // Log the error to the console
-        console.error("Error occurred:", error);
 
-        // Optionally, inform the user
-        vscode.window.showErrorMessage("An error occurred while activating the bg3txt formatter.");
-    }
+            return edits;
+        }
+    }));
+
+    // Prompt the user to optimize settings for bg3txt
+    (async function () {
+        const userChoice = await vscode.window.showInformationMessage('Would you like to optimize settings for bg3txt?', 'Yes', 'No');
+        if (userChoice === 'Yes') {
+            const config = vscode.workspace.getConfiguration();
+
+            // Check if existing settings are already in place
+            const existingTokenColorCustomizations = config.get('editor.tokenColorCustomizations') || {};
+            const existingTextMateRules = existingTokenColorCustomizations.textMateRules || [];
+            const existingFilesAssociations = config.get('files.associations') || {};
+
+            // Set the default color, unless it's already set
+            if (!existingTextMateRules.some(rule => rule.scope === 'keyword.control.bg3txt')) {
+                existingTextMateRules.push({
+                    "scope": "keyword.control.bg3txt",
+                    "settings": {
+                        "foreground": "#C586C0"
+                    }
+                });
+            }
+
+            await config.update('editor.tokenColorCustomizations', {
+                "textMateRules": existingTextMateRules
+            }, vscode.ConfigurationTarget.Workspace);
+
+            // Add file association, unless it's already set
+            if (!existingFilesAssociations['*.txt']) {
+                existingFilesAssociations['*.txt'] = "bg3txt";
+                await config.update('files.associations', existingFilesAssociations, vscode.ConfigurationTarget.Workspace);
+            }
+        }
+    })();
 }
 
 exports.activate = activate;
